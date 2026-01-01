980b190bf13453349f5196adbc9a5bf9fa5a06cf
shafiulsa <94978410+shafiulsa@users.noreply.github.com>
2026-01-01 15:50:32 +0600

    Fix authorizeRoles argument format in feedbackRoutes

--- src/routes/feedbackRoutes.js
+++ src/routes/feedbackRoutes.js
@@ -4,7 +4,7 @@ const verifyToken = require("../middleware/authMiddleware").verifyToken;
 const authorizeRoles = require("../middleware/roleMiddleware");const { getFeedbacks, createFeedback } = require("../controllers/feedbackController");
 // const { checkRole } = require("../middlewares/roleMiddleware");
 
-router.get("/", verifyToken, authorizeRoles(["admin", "client"]), getFeedbacks);
+router.get("/", verifyToken, authorizeRoles("admin", "client"), getFeedbacks);
 router.post("/", verifyToken, authorizeRoles("client"), createFeedback);
 
 module.exports = router;
